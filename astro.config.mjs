8c184c0be5d5ef1e5cfd48ab2081ce64ec1862a6
A6721jpn <netgamegeek@gmail.com>
2026-01-04 21:59:50 +0900

    Configure GitHub Pages deployment via GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import rehypeKatex from 'rehype-katex';
 // https://astro.build/config
 export default defineConfig({
   // GitHub Pages configuration
-  // site: 'https://username.github.io', 
-  // base: '/repo', 
+  site: 'https://A6721jpn.github.io',
+  base: '/portfolio',
   integrations: [
     tailwind({
       // Example: Disable default base styles if we want total control, but usually keeping them is fine.
